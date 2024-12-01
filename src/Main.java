ba6fe11457bdcc42e71cba587f93fec4b23a86ec
usb777 <dzsamoila@gmail.com>
2024-12-01 13:58:22 -0500

    mass files renamer

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,8 @@ public class Main {
         System.out.println("Total files in directory:"+fileCounter(PATH_SOURCE));
 
         fileCounter1(PATH_SOURCE);
-        fileRenamer();
+//        fileRenamer();   // is working
+        fileRenamer1();
     }
 
     private static int fileCounter(String pathFolder) {
@@ -59,6 +60,40 @@ public class Main {
         }
     }
 
+    private static void fileRenamer1()
+    {
+        String folder_path =PATH_SOURCE;
+        String prefix="";
+
+        // creating new folder
+        File myfolder = new File(folder_path);
+
+        File[] file_array = myfolder.listFiles();
+        for (int i = 0; i < file_array.length; i++)
+        {
+
+            if (file_array[i].isFile())
+            {
+
+                File myfile = new File(folder_path + file_array[i].getName());
+                String long_file_name = file_array[i].getName();
+                if( (i>=0) && (i<10))
+                {prefix="0"+i;}
+                else {prefix = String.valueOf(i);}
+
+                String new_file_name =prefix
+                        ;// tokens[1];
+                System.out.println(long_file_name);
+                System.out.print(new_file_name);
+
+                // file name format: "Snapshot 11 (12-05-2017 11-57).png"
+                // To Shorten it to "11.png", get the substring which
+                // starts after the first space character in the long
+                // _file_name.
+                myfile.renameTo(new File(PATH_OUTPUT + new_file_name + ".png"));
+            }
+        }
+    }
 
 
 
